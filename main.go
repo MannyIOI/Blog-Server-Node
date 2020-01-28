3d7391cecf89d7bd58314ae8418a0436033814fd
Amanuel Teferi <aman.teferi.80@gmail.com>
2020-01-28 19:29:24 +0300

    deleted client.go and moved it to main.go

--- main.go
+++ main.go
@@ -1,51 +1,21 @@
 package main
 
 import (
+	"blogServerNode/api"
 	"blogServerNode/models"
-	"log"
-	"net"
-	"net/http"
-	"net/rpc"
+	"blogServerNode/network"
 )
 
 func main() {
+
 	db1 := new(models.DBHandler)
 	db1.Init()
 	db1.Migrate()
-	// var reply models.User
 
-	// var user = models.User{Username: "username", Password: "password"}
-	// fmt.Println(user)
-	// db.CreateUser(user, &reply)
+	server := network.ServerNode{Address: "localhost", Port: "1237", MasterAddress: "localhost:1234"}
+	go server.ConnectToMaster()
+	go server.ExposeToMaster(db1)
 
-	// fmt.Println(reply)
-	// api.StartAPI(db, "http://localhost", 8081)
-	// name := "world"
-	// if len(os.Args) > 1 {
-	// 	name = os.Args[1]
-	// }
-	// fmt.Printf("hello, %s\n", name)
-
-	// myAddress := "localhost"
-	// serverAddress := "localhost"
-	// isMaster := os.Args[1]
-
-	// db1 := new(models.DBHandler)
-	err := rpc.Register(db1)
-
-	if err != nil {
-		log.Fatal("Format of service Task isn't correct. ", err)
-	}
-
-	rpc.HandleHTTP()
-
-	listener, e := net.Listen("tcp", ":1234")
-
-	if e != nil {
-		log.Fatal("Listen error: ", e)
-	}
-	log.Printf("Serving RPC server on port %d", 1234)
-	// Start accept incoming HTTP connections
-	err = http.Serve(listener, nil)
+	api.StartAPI(*db1, "localhost", 1238, server)
 
 }
